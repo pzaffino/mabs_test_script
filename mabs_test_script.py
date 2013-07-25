90ed7ddd27daadc64d2dcb916157026b818f333f
pzaffino <p.zaffino@unicz.it>
2013-07-25 14:57:05 +0200

    Work on configuration files creations

--- mabs_test_script.py
+++ mabs_test_script.py
@@ -9,10 +9,18 @@ import subprocess
 
 parameters={}
 
+################################################################ SECTION EDITABLE BY USER - BEGIN - ####################################################
+
 ####################################
 ### Parameters setting - BEGIN - ###
 ####################################
 
+# [PREALIGNMENT]
+parameters["reference"]=[]
+parameters["registration_config"]=[]
+parameters["spacing"]=[]
+
+# [ATLASES-SELECTION]
 parameters["enable_atlas_selection"]=[]
 parameters["atlas_selection_criteria"]=[]
 parameters["roi_mask"]=[]
@@ -21,19 +29,27 @@ parameters["mi_histogram_bins"]=[]
 parameters["lower_mi_value"]=[]
 parameters["upper_mi_value"]=[]
 
+# [TRAINING]
 parameters["atlas_dir"]=[]
 parameters["training_dir"]=[]
-
 parameters["rho_values"]=[]
 parameters["sigma_values"]=[]
 parameters["minimum_similarity"]=[]
 parameters["threshold_values"]=[]
-
 parameters["write_thresholded_files"]=[]
 parameters["write_weight_files"]=[]
 parameters["write_distance_map"]=[]
 parameters["compute_distance_map"]=[]
 
+# [REGISTRATION]
+parameters["registration_config"]=[]
+
+# [LABELING]
+parameters["input"]=[]
+parameters["output"]=[]
+
+# [STRUCTURES]
+parameters["structures"]=[]
 ####################################
 ###  Parameters setting - END -  ###
 ####################################
@@ -49,6 +65,19 @@ config_files_folder="config_files"
 ####################################
 ###    Paths setting  - END -    ###
 ####################################
+
+################################################################# SECTION EDITABLE BY USER - END - #####################################################
+
+# Assign parameters to them section
+sections = ("atlas-selection", "prealignment", "training", "registration", "labeling", "structures")
+
+prealignment_parms = ("reference", "registration_config", "spacing")
+atlases_selection_parms = ("enable_atlas_selection", "atlas_selection_criteria", "roi_mask",
+                           "mi_percent_thershold", "mi_histogram_bins", "lower_mi_value", "upper_mi_value")
+training_parms = ("atlas_dir", "training_dir", "rho_values", "sigma_values", "minimum_similarity", "threshold_values",
+                  "write_thresholded_files", "write_weight_files", "write_distance_map", "compute_distance_map")
+registration_parms = ("registration_config")
+labeling_parms = ("input", "output")
 
 # Adjust paths
 plastimatch_path.rstrip("\\/")
@@ -77,12 +106,20 @@ os.makedirs(config_files_folder)
 for i, combination in enumerate(combinations):
     fid = open(config_files_folder + os.sep + "mabs_test_" + str(i) + ".cfg", "w")
 
-    for parameter in parameters:
-        if parameters[parameter] and not parameter in non_fixed_parms:
-            fid.write(parameter + " = " + str(parameters[parameter]) +"\n")
-        elif parameters[parameter] and parameter in non_fixed_parms:
-            index = non_fixed_parms.index(parameter)
-            fid.write(parameter + " = " + str(combination[index]) +"\n")
+    for section in sections[:-1]: # No section "structures"
+        fid.write("[" + section.upper()  + "] \n"
+        
+        for parameter in parameters:
+            if parameter in vars()[section.replace("_", "-") + "_parms"]:
+                if parameters[parameter] and not parameter in non_fixed_parms:
+                    fid.write(parameter + " = " + str(parameters[parameter]) +"\n")
+                elif parameters[parameter] and parameter in non_fixed_parms:
+                    index = non_fixed_parms.index(parameter)
+                    fid.write(parameter + " = " + str(combination[index]) +"\n")
+        
+    fid.write("[STRUCTURES] \n")
+    for structure in parameters["structures"]:
+        fid.write(structure)
     
     fid.close()
 
