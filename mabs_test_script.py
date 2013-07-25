9d091c9a82eb5c09e0c827cfde92af81e34e0cea
pzaffino <p.zaffino@unicz.it>
2013-07-25 12:39:50 +0200

    First complete script

--- mabs_test_script.py
+++ mabs_test_script.py
@@ -4,6 +4,9 @@
 This is a Python script to run automatic tests on Plastimatch mabs (www.plastimatch.org)
 """
 
+import os
+import subprocess
+
 parameters={}
 
 ####################################
@@ -35,6 +38,22 @@ parameters["compute_distance_map"]=[]
 ###  Parameters setting - END -  ###
 ####################################
 
+####################################
+###   Paths setting  - BEGIN -   ###
+####################################
+
+# SET FULL PATHS
+plastimatch_path="" # if empty will be used the default installed plastimatch
+config_files_folder="config_files"
+
+####################################
+###    Paths setting  - END -    ###
+####################################
+
+# Adjust paths
+plastimatch_path.rstrip("\\/")
+config_files_folder.rstrip("\\/")
+
 # Find the non fixed parameters
 non_fixed_parms=[]
 for parameter in parameters:
@@ -53,8 +72,10 @@ for parm in non_fixed_parms:
     combinations = temp
 
 # Write parms files
+os.makedirs(config_files_folder)
+
 for i, combination in enumerate(combinations):
-    fid = open("mabs_test_"+str(i)+".cfg", "w")
+    fid = open(config_files_folder + os.sep + "mabs_test_" + str(i) + ".cfg", "w")
 
     for parameter in parameters:
         if not parameter in non_fixed_parms:
@@ -65,3 +86,15 @@ for i, combination in enumerate(combinations):
     
     fid.close()
 
+# Run plastimatch mabs
+if plastimatch_path == "":
+    plastimatch_executable = "plastimatch"
+elif plastimatch_path != "":
+    plastimatch_executable = plastimatch_path + os.sep + "plastimatch"
+
+for cfg_file in os.listdir(config_files_folder):
+    if cfg_files.endswith(".cfg"):
+        subprocess.call(plastimatch_executable + " mabs --train-registration " + config_files_folder + os.sep + cfg_files)
+        subprocess.call(plastimatch_executable + " mabs --train " + config_files_folder + os.sep + cfg_files)
+    elif not cfg_files.endswith(".cfg"):
+        print(config_files_folder + os.sep + cfg_file + " ignored \n")
